b7ce3386e4272f98f1d466f69b5a1a4241b4250b
emcahell <emmanuelltowers@gmail.com>
2025-11-05 20:43:45 -0400

    improved ui palette sidebaricon page

--- src/components/ui/sidebar.tsx
+++ src/components/ui/sidebar.tsx
@@ -266,7 +266,10 @@ function SidebarTrigger({
       data-slot="sidebar-trigger"
       variant="ghost"
       size="icon"
-      className={cn('size-7 text-white cursor-w-resize', className)}
+      className={cn(
+        'size-7 text-white bg-card-background cursor-w-resize',
+        className
+      )}
       onClick={(event: React.MouseEvent<HTMLButtonElement>) => {
         onClick?.(event);
         toggleSidebar();
